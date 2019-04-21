bd5d8ac7b0dc30312a7f27a8a0c11b724ab4bfa6
Rookie Park <rookiepark07@gmail.com>
2019-04-21 22:50:37 +1200

    Refactored formatting logic out of component Removed redundant styles in App.scss.

---
+++ credit-card-app/src/common/formatters.ts
@@ -0,0 +1,48 @@
+export function formatCardNumber(input: string) {
+  var cardNumber = input.replace(/\D/g, "");
+
+  var parts = [];
+  for (let i = 0; i < cardNumber.length; i += 4) {
+    parts.push(cardNumber.substring(i, i + 4));
+  }
+  if (parts.length) {
+    cardNumber = parts.join(" ");
+  }
+  return cardNumber;
+}
+
+export function formatCardExpiryDate(input: string) {
+  return input
+    .replace(
+      /^([1-9]\/|[2-9])$/g,
+      "0$1/" // To handle 3/ > 03/
+    )
+    .replace(
+      /^(0[1-9]{1}|1[0-2]{1})$/g,
+      "$1/" // 11 > 11/
+    )
+    .replace(
+      /^([0-1]{1})([3-9]{1})$/g,
+      "0$1/$2" // 13 > 01/3
+    )
+    .replace(
+      /^(\d)\/(\d\d)$/g,
+      "0$1/$2" // To handle 1/11 > 01/11
+    )
+    .replace(
+      /^(0?[1-9]{1}|1[0-2]{1})([0-9]{2})$/g,
+      "$1/$2" // 141 > 01/41
+    )
+    .replace(
+      /^([0]{1,})\/|[0]{1,}$/g,
+      "0" // To handle 0/ > 0 and 00 > 0
+    )
+    .replace(
+      /[^\d\/]|^[\/]{0,}$/g,
+      "" // To allow only numbers and /
+    )
+    .replace(
+      /\/\//g,
+      "/" // Prevent entering more than 1 /
+    );
+}
--- credit-card-app/src/components/CreditCardForm/CreditCardForm.tsx
+++ credit-card-app/src/components/CreditCardForm/CreditCardForm.tsx
@@ -3,59 +3,22 @@ import { Input, Button } from "antd/lib";
 import Form, { FormComponentProps } from "antd/lib/form";
 
 import "./CreditCardForm.scss";
+import {
+  formatCardExpiryDate,
+  formatCardNumber
+} from "../../common/formatters";
 
 type Props = FormComponentProps;
 
 class CreditCardForm extends Component<Props> {
   formatCardNumberField = (e: React.ChangeEvent<HTMLInputElement>) => {
-    var cardNumber = e.target.value.replace(/\D/g, "");
-
-    var parts = [];
-    for (let i = 0; i < cardNumber.length; i += 4) {
-      parts.push(cardNumber.substring(i, i + 4));
-    }
-    if (parts.length) {
-      cardNumber = parts.join(" ");
-    }
-    return cardNumber;
+    return formatCardNumber(e.target.value);
   };
 
   formatExpiryDateField = (e: React.ChangeEvent<HTMLInputElement>) => {
     let value = e.target.value;
     if ((e.nativeEvent as any).inputType != "deleteContentBackward") {
-      value = value
-        .replace(
-          /^([1-9]\/|[2-9])$/g,
-          "0$1/" // To handle 3/ > 03/
-        )
-        .replace(
-          /^(0[1-9]{1}|1[0-2]{1})$/g,
-          "$1/" // 11 > 11/
-        )
-        .replace(
-          /^([0-1]{1})([3-9]{1})$/g,
-          "0$1/$2" // 13 > 01/3
-        )
-        .replace(
-          /^(\d)\/(\d\d)$/g,
-          "0$1/$2" // To handle 1/11 > 01/11
-        )
-        .replace(
-          /^(0?[1-9]{1}|1[0-2]{1})([0-9]{2})$/g,
-          "$1/$2" // 141 > 01/41
-        )
-        .replace(
-          /^([0]{1,})\/|[0]{1,}$/g,
-          "0" // To handle 0/ > 0 and 00 > 0
-        )
-        .replace(
-          /[^\d\/]|^[\/]{0,}$/g,
-          "" // To allow only numbers and /
-        )
-        .replace(
-          /\/\//g,
-          "/" // Prevent entering more than 1 /
-        );
+      value = formatCardExpiryDate(value);
     }
     return value;
   };
