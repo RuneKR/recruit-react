e0605fd54f564f378b9d62f044cbae350e94b377
allenzhong <zhong.allen@gmail.com>
2020-09-13 07:02:47 +1200

    Fix eslint problem with ts

--- register-card-app/client/.eslintrc.js
+++ register-card-app/client/.eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
   ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
