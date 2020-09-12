8c715aa8821858ce98913735524d17ea94cda9f3
allenzhong <zhong.allen@gmail.com>
2020-09-12 18:41:40 +1200

    Add prettier

--- register-card-app/client/src/components/MenuContent/MenuContent.test.tsx
+++ register-card-app/client/src/components/MenuContent/MenuContent.test.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import MenuContent from './MenuContent';
+import React from 'react'
+import { render } from '@testing-library/react'
+import MenuContent from './MenuContent'
 
 describe('Menu should', () => {
-  it('render text', () => {
-    const { getByText } = render(<MenuContent />)
-    
-    expect(getByText('This is menu content')).toBeTruthy();
-  })
+    it('render text', () => {
+        const { getByText } = render(<MenuContent />)
+
+        expect(getByText('This is menu content')).toBeTruthy()
+    })
 })
