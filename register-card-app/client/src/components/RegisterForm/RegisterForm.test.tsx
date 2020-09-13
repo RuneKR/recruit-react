253507a74432fcc4de2acc117f177497de0f448d
allenzhong <zhong.allen@gmail.com>
2020-09-13 14:55:29 +1200

    Refactor testing code

--- register-card-app/client/src/components/RegisterForm/RegisterForm.test.tsx
+++ register-card-app/client/src/components/RegisterForm/RegisterForm.test.tsx
@@ -3,14 +3,14 @@ import { render, RenderResult, fireEvent, act } from '@testing-library/react'
 import RegisterForm, { RegisterFormProps, CreditCard } from './RegisterForm'
 
 describe('RegisterForm', () => {
-  const onSubmitCallback: (data: CreditCard) => Promise<void> = jest.fn();
-  
+  const onSubmitCallback: (data: CreditCard) => Promise<void> = jest.fn()
+
   const renderRegisterForm = (): RenderResult => {
     const props: RegisterFormProps = {
-      onSubmitCallback
+      onSubmitCallback,
     }
-    return render(<RegisterForm {...props}/>)
-  } 
+    return render(<RegisterForm {...props} />)
+  }
 
   it('should render cardNumber field', () => {
     const { getByTestId } = renderRegisterForm()
@@ -36,32 +36,43 @@ describe('RegisterForm', () => {
   })
 
   it('should be able to submit data when data is valid', async () => {
-
     const { container, getByTestId } = renderRegisterForm()
-    const cardNumberInput = container.querySelector('input#input-cardNumber')
-    const cvcInput = container.querySelector('input#input-cvc')
-    const expiryDateInput = container.querySelector('input#input-expiryDate')
 
-
-    if(cardNumberInput !== null
-      && cvcInput !== null
-      && expiryDateInput !== null) {
-      fireEvent.change(cardNumberInput, { target: { value: '4929139878109731' }}) 
-      fireEvent.change(cvcInput, { target: { value: '132' }}) 
-      fireEvent.change(expiryDateInput, { target: { value: '0423' }})
+    const expectedParameter: CreditCard = {
+      cardNumber: '4929139878109731',
+      cvcNumber: '132',
+      expiryDate: '0423',
     }
+
+    fireChangeEvent(container, expectedParameter)
 
     await act(async () => {
       fireEvent.submit(getByTestId('form'))
     })
 
-    const expectedParameter: CreditCard = {
-      cardNumber: '4929139878109731',
-      cvcNumber: '132',
-      expiryDate: '0423'
-    } 
-
     expect(onSubmitCallback).toBeCalledWith(expectedParameter)
-
   })
+
+
+  function fireChangeEvent(container: HTMLElement, expectedParameter: CreditCard): void {
+    const cardNumberInput = container.querySelector('input#input-cardNumber')
+    const cvcInput = container.querySelector('input#input-cvc')
+    const expiryDateInput = container.querySelector('input#input-expiryDate')
+
+    if (cardNumberInput !== null &&
+      cvcInput !== null &&
+      expiryDateInput !== null) {
+      fireEvent.change(cardNumberInput, {
+        target: { value: expectedParameter.cardNumber },
+      })
+      fireEvent.change(cvcInput, {
+        target: { value: expectedParameter.cvcNumber },
+      })
+      fireEvent.change(expiryDateInput, {
+        target: { value: expectedParameter.expiryDate },
+      })
+    }
+  }
 })
+
+
